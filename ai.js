7a52dbe6fc8cc51f80fc5ae3b5666969f15aafb7
bae43 <bae43@cornell.edu>
2015-11-09 07:27:42 +0000

    added ai.js

---
+++ ai.js
@@ -0,0 +1,31 @@
+/**
+ * A class for suggested moves and computer opponents
+ */
+
+module.exports = AI;
+
+AI = function(campus, state) {
+  this.campus = {};
+  this.state = {}; 
+
+}
+AI.prototype = {
+  /**
+   * Randomly grab a building
+   */
+  getGrabMoveNaive : function(team) {
+  
+  },
+  /**
+   * Distribute reinforcements along border
+   */
+  getReinforcementsMoveNaive : function(team){
+
+  },
+  /**
+   * Attack anything and everything
+   */
+  getOrdersMoveNaive : function(team) {
+
+  },
+}
